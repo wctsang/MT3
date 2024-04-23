948f13f552910756e7b4b58352ac77fde630aba4
wctsang <147121595+wctsang@users.noreply.github.com>
2024-04-23 14:35:38 +0900

    MT3_00_01

--- KamataEngine/Novice/main.cpp
+++ KamataEngine/Novice/main.cpp
@@ -1,12 +1,92 @@
+#define _USE_MATH_DEFINES
 #include <Novice.h>
+#include <assert.h>
+#include <math.h>
 
-const char kWindowTitle[] = "学籍番号";
+// 構造体宣言
+
+struct Vector3 {
+	float x, y, z;
+};
+
+// 関数宣言
+
+// 加算
+
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return result;
+}
+
+// 減算
+
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return result;
+}
+
+// スカラー倍
+
+Vector3 Multiply(float scalar, const Vector3& v) {
+	Vector3 result;
+	result.x = v.x * scalar;
+	result.y = v.y * scalar;
+	result.z = v.z * scalar;
+	return result;
+}
+
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+	float result;
+	result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	return result;
+}
+
+// 長さ（ノルム）
+float Length(const Vector3& v) {
+	float result;
+	result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return result;
+}
+
+// 正規化
+Vector3 Normalise(const Vector3& v) {
+	Vector3 result;
+	float magnitude = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	result.x = v.x / magnitude;
+	result.y = v.y / magnitude;
+	result.z = v.z / magnitude;
+	return result;
+}
+
+// 数値表示
+
+static const int kColumnWidth = 60;
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+const char kWindowTitle[] = "GC2C_05_ソウ_イチョウ_MT3";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 v1{1.0f, 3.0f, -5.0f};
+	Vector3 v2{4.0f, -1.0f, 2.0f};
+	float k = {4.0f};
+	int kRowHeight = 20;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -25,6 +105,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalise = Normalise(v2);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +119,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, resultAdd, ": Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, ": Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, ": Multiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalise, ": Normalise");
 
 		///
 		/// ↑描画処理ここまで
